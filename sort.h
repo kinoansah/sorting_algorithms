7ed28038d04ea15334bc75fa73fe89952ac1a90d
kinoansah <kinoansah@gmail.com>
2023-07-19 09:10:37 -0700

    betty corrections on header file

--- sort.h
+++ sort.h
@@ -4,11 +4,18 @@
 #include <stddef.h>
 
 /* Doubly linked list node */
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
 typedef struct listint_s
 {
-	const int n;
-	struct listint_s *prev;
-	struct listint_s *next;
+	const int n;			/* Integer stored in the node */
+	struct listint_s *prev;	/* Pointer to the previous element of the list */
+	struct listint_s *next;	/* Pointer to the next element of the list */
 } listint_t;
 
 /* Function prototypes */
